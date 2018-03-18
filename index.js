e471beb503da6e9910dd66b1300c4cbef0e1c1bf
Joffrey <teenrock@users.noreply.github.com>
2018-03-18 19:08:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const Discord = require("./node_modules/discord.js");
 const bot = new Discord.Client({autoReconnect: true, max_message_cache: 0});
 const config = require("./conf.json");
-const prefix = config.prefix;
 /////////////////////////////////////////////////////////////////////////////////////////
 
 bot.on("ready", function() {
@@ -22,25 +21,28 @@ var serverB_chan = bot.channels.get('423450887986479107'); // Server_chanB Chann
 var serverC_chan = bot.channels.get('423450986506485762'); // Server_chanC ChannelID
 var botID = bot.users.get('423458755397681152');
 var botDMs = `Msg de **${message.author.username}** sur <@${bot.user.id}> : ${message}`;
+var userMSG = `**${message.author.username}** : ${message}`;
+// Bot DMs Channels Return 
 	if (message.channel.name === undefined) {
 		serverA_chan.send(botDMs);
 		serverB_chan.send(botDMs);
 		serverC_chan.send(botDMs);
 	}
+// Discord ServerA/B/C Channels link
 	if (message.channel === serverA_chan) {
 	if (message.author === botID) return;
-		serverB_chan.send(`**${message.author.username}** : ${message}`);
-		serverC_chan.send(`**${message.author.username}** : ${message}`);
+		serverB_chan.send(userMSG);
+		serverC_chan.send(userMSG);
 	}
 	if (message.channel === serverB_chan) {
 	if (message.author === botID) return;
-		serverA_chan.send(`**${message.author.username}** : ${message}`);
-		serverC_chan.send(`**${message.author.username}** : ${message}`);
+		serverA_chan.send(userMSG);
+		serverC_chan.send(userMSG);
 	}
 	if (message.channel === serverC_chan) {
 	if (message.author === botID) return;
-		serverA_chan.send(`**${message.author.username}** : ${message}`);
-		serverB_chan.send(`**${message.author.username}** : ${message}`);
+		serverA_chan.send(userMSG);
+		serverB_chan.send(userMSG);
 	}
 });
 
